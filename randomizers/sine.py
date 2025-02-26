84fd95015e44a83d22784df190810447457fd192
Richard A. Dubniczky <richard@dubniczky.com>
2025-02-26 23:07:53 +0100

    Added comments for the sine random generator

--- randomizers/sine.py
+++ randomizers/sine.py
@@ -1,8 +1,10 @@
 import math
 import time
 
-
+# The base seed is always added to the seed that is set randommly or by the user to provide additional randomness.
 base_seed = 1976123.5081254
+# A static number that is generated based on the time when the program is run.
+# This is weak to timing attacks, so never use it for secure random number generation.
 global_seed = base_seed + int(time.time() * 1000)
 global_step = 0
 
@@ -17,7 +19,7 @@ def random() -> float:
     global global_seed, global_step
     global_step += 1
     num = (math.sin(global_seed + global_step)+1) * 43659.98891672314
-    return num - int(num)
+    return num - int(num) # Return the fractional part of the number
 
 def range(start: int, end: int) -> int:
     '''Generate a random integer between start and end, both inclusive.'''
